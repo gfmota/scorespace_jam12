cd1ae591092230af8d0c6310b178a3c71064f06d
Gabriel Mota <gabrielfmota134@gmail.com>
2021-03-19 20:24:19 -0300

    Remove staff and add energies/trading energies and its rotation
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -3,12 +3,21 @@ extends KinematicBody2D
 const ACCELERATION : int = 1500
 const MAX_SPEED : int = 2500
 const FRICTION : float = 0.9
+onready var casting : bool = false
+onready var act_energy : int = 0
+onready var energies_node : Node2D = $Energies
+onready var energies : Array = [$Energies/Energy1, $Energies/Energy2, $Energies/Energy3]
 onready var speed : Vector2 = Vector2.ZERO
+onready var spell_a_cooldown : Timer = $CooldownA
+onready var spell_b_cooldown : Timer = $CooldownB
 onready var sprite : AnimatedSprite = $Sprite
-onready var staff : Node2D = $Staff
+onready var tween : Tween = $Tween
 
 func _ready():
-	pass
+	tween.interpolate_property(energies_node, "rotation_degrees", 0, 360, 2)
+	for energy in energies:
+		tween.interpolate_property(energy, "rotation_degrees", 0, -360, 2)
+	tween.start()
 
 func _physics_process(delta):
 	var direction : Vector2 = Vector2.ZERO
@@ -32,10 +41,28 @@ func _physics_process(delta):
 			sprite.play("run")
 	
 	var mouse_pos : Vector2 = get_global_mouse_position()
-	staff.look_at(mouse_pos)
-	if mouse_pos.x - global_position.x < 0 and not staff.get_node("Sprite").flip_v:
-		staff.get_node("Sprite").flip_v = true
+	if mouse_pos.x - global_position.x < 0 and not sprite.flip_h:
 		sprite.flip_h = true
-	elif mouse_pos.x - global_position.x > 0 and staff.get_node("Sprite").flip_v:
-		staff.get_node("Sprite").flip_v = false
+	elif mouse_pos.x - global_position.x > 0 and sprite.flip_h:
 		sprite.flip_h = false
+
+func _input(event):
+	if event.is_action_pressed("left_mouse") and spell_a_cooldown.is_stopped():
+		# Cast spell A
+		spell_a_cooldown.start()
+	if event.is_action_pressed("right_mouse") and spell_b_cooldown.is_stopped():
+		#Cast spell B
+		spell_b_cooldown.start()
+	
+	if event.is_action_pressed("green_energy"):
+		energy_manager("green")
+	if event.is_action_pressed("blue_energy"):
+		energy_manager("blue")
+	if event.is_action_pressed("red_energy"):
+		energy_manager("red")
+
+func energy_manager(color):
+	energies[act_energy].play(color)
+	act_energy += 1
+	if act_energy == 3:
+		act_energy = 0
